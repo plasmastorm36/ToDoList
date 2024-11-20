330151e75dc2c5e6a64be44c45354a936c7d9866
plasmastorm36 <plasmastormfire@gmail.com>
2024-11-20 12:29:27 -0500

    Added task entity

---
+++ main/src/main/java/com/todolist/main/model/Task.java
@@ -0,0 +1,101 @@
+package com.todolist.main.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDate;
+
+@Entity
+/**
+ * Task entity made to store task data. Annotated to tell mvn how to store it in the db
+ * @author Noah Rouse
+ * @email noahrouse36@gmail.com
+ */
+public class Task {
+   @Id
+   @GeneratedValue(strategy = GenerationType.AUTO)
+   @Column(name= "Id")
+   private long id;
+
+   @Column(name = "Description")
+   private String description;
+
+   @Column(name = "Priority")
+   private String priority;
+
+   @Column(name = "Due Date")
+   private LocalDate dueDate;
+
+   @Column(name = "Completed")
+   private boolean isCompleted;
+
+   @Column(name = "Created")
+   private LocalDate created;
+   
+   @Column(name = "Updated")
+   private LocalDate lastUpdated;
+
+   public Task () {}
+
+   public Task (final String description, final String priority, final LocalDate dueDate) {
+      this.description = description;
+      this.priority = priority;
+      this.dueDate = dueDate;
+      this.isCompleted = false;
+      created = LocalDate.now();
+      lastUpdated = created;
+   }
+
+   public Long getId () {
+      return this.id;
+   }
+
+   public String getDescription () {
+      return this.description;
+   }
+
+   public String getPriority () {
+      return this.priority;
+   }
+
+   public LocalDate getDueDate () {
+      return this.dueDate;
+   }
+
+   public boolean getIsCompleted () {
+      return this.isCompleted;
+   }
+
+   public LocalDate getCreated () {
+      return this.created;
+   }
+
+   public LocalDate getLastUpdated () {
+      return this.lastUpdated;
+   }
+
+   public void setDescription (final String description) {
+      this.description = description;
+      lastUpdated = LocalDate.now();
+   }
+
+   public void setPriority (final String priority) {
+      this.priority = priority;
+      lastUpdated = LocalDate.now();
+   }
+
+   public void setDueDate (final LocalDate dueDate) {
+      this.dueDate = dueDate;
+      lastUpdated = LocalDate.now();
+   }
+
+   public void setIsCompleted (final boolean isCompleted) {
+      this.isCompleted = isCompleted;
+      lastUpdated = LocalDate.now();
+   }
+}
