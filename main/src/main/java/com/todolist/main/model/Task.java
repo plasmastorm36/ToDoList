f5b75f83036012bc93bcb3dd592445667fe2b000
plasmastorm36 <plasmastormfire@gmail.com>
2024-11-20 12:55:47 -0500

    Removed an unnecessary import from Task.java

--- main/src/main/java/com/todolist/main/model/Task.java
+++ main/src/main/java/com/todolist/main/model/Task.java
@@ -1,7 +1,5 @@
 package com.todolist.main.model;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
