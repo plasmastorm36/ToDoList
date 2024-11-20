bf18dc2fc45db0a6db0a777d30a2b45222953dda
plasmastorm36 <plasmastormfire@gmail.com>
2024-11-20 14:50:09 -0500

    fixed a query in Task Repository

--- main/src/main/java/com/todolist/main/repository/TaskRepository.java
+++ main/src/main/java/com/todolist/main/repository/TaskRepository.java
@@ -22,7 +22,7 @@ public interface TaskRepository extends JpaRepository<Task, Long> {
 
    public Optional<Task> findById (Long id);
 
-   @Query("SELECT t FROM Tasks WHERE t.isCompleted = :isCompleted")
+   @Query("SELECT t FROM Tasks WHERE t.Completed = :isCompleted")
    public List<Task> findByCompletion (boolean isCompleted);
 
    @Query("SELECT t FROM Tasks WHERE t.dueDate = :dueDate")
